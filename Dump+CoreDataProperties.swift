a5d7874aa58fcad5b99d0004258c64447acf2881
Kyle Lee <kiloloco@me.com>
2015-11-28 23:40:56 -0800

    finished scrolling view, setup categoriesVC basics, setup categories segue

---
+++ Dump+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Dump+CoreDataProperties.swift
+//  BrainDump
+//
+//  Created by Kyle Lee on 11/28/15.
+//  Copyright © 2015 Kyle Lee. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Dump {
+
+    @NSManaged var note: String?
+    @NSManaged var date: NSDate?
+    @NSManaged var category: String?
+    @NSManaged var title: String?
+
+}
---
+++ Dump.swift
@@ -0,0 +1,17 @@
+//
+//  Dump.swift
+//  BrainDump
+//
+//  Created by Kyle Lee on 11/28/15.
+//  Copyright © 2015 Kyle Lee. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Dump: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
