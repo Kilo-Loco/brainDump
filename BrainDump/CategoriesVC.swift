2a8db769d16ab231db0e663947d464a322ada65b
Kyle Lee <kiloloco@me.com>
2015-11-30 23:17:18 -0800

    finished basics for core data and got categoriesVC working

--- BrainDump/CategoriesVC.swift
+++ BrainDump/CategoriesVC.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
-class CategoriesVC: UIViewController {
+class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var dumps = [Dump]()
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent
@@ -16,6 +20,14 @@ class CategoriesVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.fetchAndSetResult()
+        self.tableView.reloadData()
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -23,6 +35,38 @@ class CategoriesVC: UIViewController {
     
     @IBAction func backBtnPressed(sender: UIButton) {
         performSegueWithIdentifier("CategoriesToVC", sender: nil)
+    }
+    
+    func fetchAndSetResult() {
+        let app = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context = app.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName: "Dump")
+        
+        do {
+            let results = try context.executeFetchRequest(fetchRequest)
+            self.dumps = results as! [Dump]
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCellWithIdentifier("DumpCell") as? DumpCell {
+            let dump = dumps[indexPath.row]
+            cell.configureCell(dump)
+            return cell
+        } else {
+            return DumpCell()
+        }
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dumps.count
     }
     
 }
---
+++ BrainDump/DumpCell.swift
@@ -0,0 +1,35 @@
+//
+//  DumpCell.swift
+//  BrainDump
+//
+//  Created by Kyle Lee on 11/30/15.
+//  Copyright © 2015 Kyle Lee. All rights reserved.
+//
+
+import UIKit
+
+class DumpCell: UITableViewCell {
+    
+    @IBOutlet weak var dumpTitle: UILabel!
+    @IBOutlet weak var dumpNote: UILabel!
+    @IBOutlet weak var dumpDate: UILabel!
+    @IBOutlet weak var dumpCategoryColor: UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func configureCell(dump: Dump) {
+        self.dumpTitle.text = dump.note
+        self.dumpNote.text = dump.note
+        self.dumpDate.text = NSDateFormatter.localizedStringFromDate(dump.date!, dateStyle: NSDateFormatterStyle.ShortStyle, timeStyle: NSDateFormatterStyle.ShortStyle)
+        self.dumpCategoryColor.backgroundColor = UIColor.init(colorLiteralRed: 233.0, green: 30.0, blue: 99.0, alpha: 1.0)
+    }
+
+//    override func setSelected(selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
