6845d5ba9bf511388ad68e037f23b439c3c10cde
Kyle Lee <kiloloco@me.com>
2015-12-26 23:54:46 -0800

    Added Apple Watch Extension

---
+++ BrainDumpWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  BrainDumpWatch Extension
+//
+//  Created by Kyle Lee on 12/26/15.
+//  Copyright © 2015 Kyle Lee. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
